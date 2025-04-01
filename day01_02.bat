9233851439d12d1cad37e8a49f6c720d0483c732
Sithumi Hirushika Kadigamuwage <sithumikadigamuwage@gmail.com>
2025-04-01 16:12:56 +0530

    Add files via upload

---
+++ day01_02.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Username: %username%
+echo Windows version: 
+ver
+color b
+time
+date
+dir/w
+echo Day:%date%
+pause
---
+++ day_01.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Birthday calculator
+set /p birth_year = Enter your birth year:
+echo %date%
+pause
